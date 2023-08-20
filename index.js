052cf23ed6cf6f98c56c392eefb3c04db17a1ab9
haku-ohsumi <haku818181@gmail.com>
2023-08-20 21:45:48 +0900

    ブラウザでデータを表示できるよ

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ app.use(express.json())
 const connectDB = require("./utils/database")
 const { ItemModel } = require("./utils/schemaModels")
 
-app.get("/", (req, res) => {
-  return res.status(200).json("こんにちは")
-})
-
 //ITEM functionsだよ
 //Create Itemだよ
 app.post("/item/create", async(req, res) => {
@@ -23,6 +19,15 @@ app.post("/item/create", async(req, res) => {
 })
 
 //Read All Items
+app.get("/", async(req, res) => {
+  try{
+    await connectDB()
+    const allItems = await ItemModel.find()
+    return res.status(200).json({message: "アイテムの読み取り成功（オール）", allItems: allItems})
+  }catch(err){
+    return res.status(400).json({message: "アイテムの読み取り失敗（オール）"})
+  }
+})
 //Read Single Item
 //Update Item
 //Delete Item
