4966b43d0b2485cf7ab2638d9336fb548368b465
haku-ohsumi <haku818181@gmail.com>
2023-08-20 21:10:51 +0900

    データベースと接続したよ

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express()
 const port = 5050;
 app.use(express.urlencoded({ extended:true }))
 app.use(express.json())
+const connectDB = require("./utils/database")
+const { ItemModel } = require("./utils/schemaModels")
 
 app.get("/", (req, res) => {
   return res.status(200).json("こんにちは")
@@ -10,9 +12,14 @@ app.get("/", (req, res) => {
 
 //ITEM functionsだよ
 //Create Itemだよ
-app.post("/item/create", (req, res) => {
-  console.log(req.body)
-  return res.status(200).json("ばいばい")
+app.post("/item/create", async(req, res) => {
+  try {
+  connectDB()
+  await ItemModel.create(req.body)
+  return res.status(200).json({message: "アイテム作成成功"})
+  }catch(err){
+    return res.status(400).json({message: "アイテム作成失敗"})
+  }
 })
 
 //Read All Items
